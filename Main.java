4a8a7a3706fa641e96902f9fbb85d4e39c1517c6
Yuri Goncharov <ygpanzer@gmail.com>
2021-07-03 13:26:30 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Prints Monkey or Cat first!!!
+
+        Monkey monkey = new Monkey();
+        //monkey.run();
+
+        Cat cat = new Cat();
+        //cat.run();
+
+        Thread thread = new Thread(monkey);
+        thread.start();
+
+        Thread thread2 = new Thread(cat);
+        thread2.start();
+
+    }
+}
